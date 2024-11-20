28e927fb7ac5768af7069edacbbaf6f893bfa3d9
laisimiao <2625482530@qq.com>
2024-11-20 09:46:25 +0800

    fix typos: ostrack->lorat

--- ytest.sh
+++ ytest.sh
@@ -4,8 +4,8 @@ python tracking/analysis_results.py # need to modify tracker configs and names
 
 # GOT10K-test
 python tracking/test.py lorat base_224 --dataset got10k_test --threads 2 --num_gpus 2
-python lib/test/utils/transform_got10k.py --tracker_name ostrack --cfg_name base_224_300
+python lib/test/utils/transform_got10k.py --tracker_name lorat --cfg_name base_224_300
 
 # TrackingNet
 python tracking/test.py lorat base_224 --dataset trackingnet --threads 2 --num_gpus 2
-python lib/test/utils/transform_trackingnet.py --tracker_name ostrack --cfg_name base_224_300
+python lib/test/utils/transform_trackingnet.py --tracker_name lorat --cfg_name base_224_300
